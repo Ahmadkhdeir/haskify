8b3b305017d4b12a911d23e8ea010be587896b7c
Ahmadkhdeir <ahmadkhdier@icloud.com>
2025-05-16 20:09:04 +0200

    make title and logo clickable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 
 export default function Header() {
   return (
     <header className="haskify-header">
       <div className="header-content">
         <div className="logo-container">
-          <img src={logo} alt="Haskify Logo" className="logo" />
-          <span className="app-name">Haskify</span>
+          <Link to="/" className="logo-container" style={{ textDecoration: 'none' }}>
+            <img src={logo} alt="Haskify Logo" className="logo" />
+            <span className="app-name">Haskify</span>
+          </Link>
         </div>
         <nav className="header-nav">
           <a href="/" className="nav-link">Home</a>
